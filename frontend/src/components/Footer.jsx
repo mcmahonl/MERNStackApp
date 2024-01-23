97784860c29a7751defc7812ad77a93abb2c95f3
Lance McMahon <mcmahon.lancej@gmail.com>
2024-01-23 02:46:06 -0500

    Added product component and rendered it on homescreen with dummy data on frontend

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className="text-center py-3">
-            <p>MERN Stack Store App &copy; {currentYear}</p>
+            <p>Ecommerce Store &copy; {currentYear}</p>
           </Col>
         </Row>
       </Container>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">MERN Stack Store App</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="logo" width="30" height="30" className="d-inline-block align-top" />{' '}
+            Ecommerce Store
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
