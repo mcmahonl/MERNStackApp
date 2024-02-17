2f6cae04549e2a23af9dea63fa9a35ffffb18d82
Lance McMahon <mcmahon.lancej@gmail.com>
2024-02-16 20:01:43 -0500

    Section 9 video 1: shipping route implementation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,7 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -20,6 +21,7 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      <Route path="/shipping" element={<ShippingScreen />} />
     </Route>
   )
 );
