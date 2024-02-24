927fbc75a95cb984ce0cb74bc43a1e840e918eda
Lance McMahon <mcmahon.lancej@gmail.com>
2024-02-23 22:06:26 -0500

    Finished Section 10 Videos: Full-stack implementation of orders, checkout process completed, Paypal sandbox integrated for testing

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -66,32 +66,49 @@ const getOrderById = asyncHandler(async (req, res) => {
 });
 
 // @desc    Update order to paid
-// @route   PUT /api/orders/:id/pay
+// @route   GET /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send('updateOrderToPaid');
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
 // @desc    Update order to delivered
 // @route   GET /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
-  res.send('updateOrderToDelivered');
+  res.send('update order to delivered');
 });
 
 // @desc    Get all orders
 // @route   GET /api/orders
 // @access  Private/Admin
 const getOrders = asyncHandler(async (req, res) => {
-  res.send('getOrders');
+  res.send('get all orders');
 });
 
 export {
   addOrderItems,
+  getMyOrders,
   getOrderById,
   updateOrderToPaid,
   updateOrderToDelivered,
-  getMyOrders,
   getOrders,
 };
-
