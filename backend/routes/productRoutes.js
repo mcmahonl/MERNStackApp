9ce07f90b2ee6d5bcebf5ad0b625e3ee68a90b63
Lance McMahon <mcmahon.lancej@gmail.com>
2024-03-26 01:48:30 -0400

    Implemented backend behavior for product reviews in controllers and routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,5 +8,6 @@ router.route('/:id')
 .get(getProductById)
 .put(protect, admin, updateProduct)
 .delete(protect, admin, deleteProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
